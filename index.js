d549f287b1bb3d8bf5011e7ead2d2e5ee5bb3000
sharafuddin3526-dotcom <sharafuddin3526@gmail.com>
2026-05-04 19:34:09 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.start((ctx) => {
+  ctx.reply("👋 Bot is running on GitHub + Railway!");
+});
+
+bot.launch();
+
+console.log("Bot started...");
