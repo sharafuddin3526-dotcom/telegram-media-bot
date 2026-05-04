4af469eba2a480b4f24043c49cad66901b32a1bd
sharafuddin3526-dotcom <sharafuddin3526@gmail.com>
2026-05-04 20:25:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const { Telegraf } = require("telegraf");
 const axios = require("axios");
 const config = require("./config");
 
-// use config token instead of env
 const bot = new Telegraf(config.BOT_TOKEN);
 
-// start message
 bot.start((ctx) => {
   ctx.reply(
     "👋 Send me TikTok / Facebook / Instagram video link\n\n📥 I will try to download it!"
   );
 });
 
-// helper function (API call)
 async function getVideo(url) {
   try {
     const api = `https://api.tiklydown.me/api/download?url=${encodeURIComponent(url)}`;
@@ -23,7 +20,6 @@ async function getVideo(url) {
   }
 }
 
-// message handler
 bot.on("text", async (ctx) => {
   const url = ctx.message.text;
 
@@ -36,13 +32,12 @@ bot.on("text", async (ctx) => {
   const data = await getVideo(url);
 
   if (!data || !data.video) {
-    return ctx.reply("❌ Failed to download video. Try another link.");
+    return ctx.reply("❌ Failed to download video. Try another link!");
   }
 
   return ctx.replyWithVideo(data.video);
 });
 
-// error safety (important)
 bot.catch((err) => {
   console.log("Bot error:", err);
 });
